a7d10391b039dbd2faf7f8f7b381a77ca0417db1
unknown <sonja.armio@gmail.com>
2022-02-05 21:53:31 +0200

    1.6 step 1

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { MouseEventHandler, useState } from 'react';
 
-function App() {
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const header1: string = 'give feedback';
+  const header2: string = 'statics';
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+  const handleNaturalClick = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header header={header1}></Header>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNaturalClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Header header={header2}></Header>
+      good: {good} <br />
+      neutral: {neutral} <br />
+      bad: {bad}
     </div>
   );
-}
+};
+const Header = (props: { header: string }) => {
+  return <h1>{props.header}</h1>;
+};
+
+const Button = (props: {
+  handleClick: MouseEventHandler<HTMLButtonElement>;
+  text: string;
+}) => {
+  console.log(props);
+  const { handleClick, text } = props;
+  return <button onClick={handleClick}>{text}</button>;
+};
 
 export default App;
